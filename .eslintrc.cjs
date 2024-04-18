378f4a3a58a2ba67258ce6bf94d1aade13eb364e
stavrogyn <nikitakrugovoy@gmail.com>
2024-04-19 00:54:52 +0200

    chore: correct eslint + install deps + add asdf

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,57 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended', 'plugin:import/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    '@typescript-eslint/naming-convention': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/default': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'import/order': [
+          'warn',
+          {
+            groups: [
+              ['builtin', 'external', 'internal'],
+              ['parent', 'sibling', 'index'],
+            ],
+            pathGroups: [
+              {
+                pattern: '*.css',
+                patternOptions: { matchBase: true },
+                group: 'sibling',
+                position: 'after',
+              },
+              {
+                pattern: 'shared/**',
+                patternOptions: { matchBase: true },
+                group: 'internal',
+                position: 'after',
+              },
+            ],
+            'newlines-between': 'always',
+          },
+        ],
+      },
+    },
+    {
+      files: ['*.stories.tsx'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'import/order': 'off',
+        'import/no-unresolved': 'off',
+      },
+    },
+  ],
 }
